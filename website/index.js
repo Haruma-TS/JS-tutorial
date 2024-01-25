79337f92aecb5fc40d68bb638e146c8958c42cc5
Haruma-TS <mrnakezz@gmail.com>
2024-01-25 22:51:48 +0100

    random button

--- website/index.js
+++ website/index.js
@@ -1,19 +1,10 @@
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum;
 
-let count = 0;
-
-increaseBtn.onclick = function () {
-  count++;
-  countLabel.textContent = count;
-};
-resetBtn.onclick = function () {
-  count = 0;
-  countLabel.textContent = count;
-};
-decreaseBtn.onclick = function () {
-  count--;
-  countLabel.textContent = count;
+myButton.onclick = function () {
+  randomNum = Math.floor(Math.random() * max) + min;
+  myLabel.textContent = randomNum;
 };
