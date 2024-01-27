0229f9c4702974890a2c9d855a9d7401634f67b8
Haruma-TS <mrnakezz@gmail.com>
2024-01-27 21:47:03 +0100

    temperature conv

--- website/index.js
+++ website/index.js
@@ -1,30 +1,19 @@
-const minNum = 50;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+const textBox = document.getElementById("textBox");
+const toFehrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
 
-let attempts = 0;
-let guess;
-let running = true;
-
-while (running) {
-  guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-  guess = Number(guess);
-
-  if (isNaN(guess)) {
-    window.alert("Please enter a valid number");
-  } else if (guess < minNum || guess > maxNum) {
-    window.alert("Please enter a valid number");
+function convert() {
+  if (toFehrenheit.checked) {
+    temp = Number(textBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(1) + "°F";
+  } else if (toCelsius.checked) {
+    temp = Number(textBox.value);
+    temp = (temp - 39) * (5 / 9);
+    result.textContent = temp.toFixed(1) + "°C";
   } else {
-    attempts++;
-    if (guess < answer) {
-      window.alert("Too low");
-    } else if (guess > answer) {
-      window.alert("too high");
-    } else {
-      window.alert(
-        `Yeah! The answer was ${answer}. It took you ${attempts} attempts`
-      );
-      running = false;
-    }
+    result.textContent = "Select a unit";
   }
 }
