a2e8cc7e9f05817e3ec9c5a34b609cf088f4126b
Haruma-TS <mrnakezz@gmail.com>
2024-01-30 20:13:09 +0100

    Clock

--- website/index.js
+++ website/index.js
@@ -1,47 +1,11 @@
-function generatePassword(
-  passwordLength,
-  includeUpperCase,
-  includeLowerCase,
-  includeNumbers,
-  includeSymbols
-) {
-  const upperCaseChars = "ABCDEFGHIGKLMNOPQRSTUVWXYZ";
-  const lowerCaseChars = upperCaseChars.toLowerCase();
-  const numberChars = "0123456789";
-  const symbolChars = "!@#$%^&*()_-=";
-
-  let allowedChars = "";
-  let password = "";
-  allowedChars += includeUpperCase ? upperCaseChars : "";
-  allowedChars += includeLowerCase ? lowerCaseChars : "";
-  allowedChars += includeNumbers ? numberChars : "";
-  allowedChars += includeSymbols ? symbolChars : "";
-
-  if (passwordLength <= 0) {
-    return "(password length must be at least 1)";
-  }
-
-  if (allowedChars.length === 0) {
-    return "(At least 1 set of characters needs to be selected)";
-  }
-  for (let i = 0; i < passwordLength; i++) {
-    const randomIndex = Math.floor(Math.random() * allowedChars.length);
-    password += allowedChars[randomIndex];
-  }
-  return password;
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, 0);
+  const minutes = now.getMinutes().toString().padStart(2, 0);
+  const seconds = now.getSeconds().toString().padStart(2, 0);
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").textContent = timeString;
 }
-const passwordLength = 12;
-const includeUpperCase = true;
-const includeLowerCase = true;
-const includeNumbers = true;
-const includeSymbols = true;
 
-const password = generatePassword(
-  passwordLength,
-  includeUpperCase,
-  includeLowerCase,
-  includeNumbers,
-  includeSymbols
-);
-
-console.log(`Generated password: ${password}`);
+updateClock();
+setInterval(updateClock, 100);
